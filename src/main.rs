186c23b70a5dc510afc0371a638fe17c37c69c08
abanoubL <131357493+abanoubL@users.noreply.github.com>
2024-01-05 02:54:38 +0000

    trying github actions workflows

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
+use glob::glob;
 use serde::Deserialize;
 use serde_json;
-use std::{fs, f32::consts::E, vec};
-use glob::glob;
 use std::os::unix::fs::PermissionsExt;
+use std::{f32::consts::E, fs, vec};
 
 // load the regex-fules.json file to provide configs
 const JSON: &str = include_str!("../rules.json");
-
 
 #[derive(Deserialize, Debug)]
 struct ComplianceRule {
@@ -41,37 +40,5 @@ fn load_rules() -> Vec<ComplianceRule> {
 }
 
 fn main() {
-    let rules = load_rules(); 
-    let mut failed: bool = false;
-    for rule in rules {
-        let mut seen_files: Vec<String> = Vec::new();
-        for entry in glob(&rule.path_regex).expect("Failed to read glob pattern") {
-            match entry {
-                Ok(path) => {
-                    if path.is_dir() {
-                        continue;
-                    }
-                    seen_files.push(path.to_str().unwrap().to_string());
-                    let metadata = fs::metadata(&path).unwrap();
-                    if metadata.permissions().mode() != rule.file_permissions {
-                        failed = true;
-                        println!("[FAIL] incorrect permissions: {:?}", path);
-                    }
-
-
-                }
-                Err(e) => println!("{:?}", e),
-            }
-        }
-        for file in &rule.required_files {
-            if !seen_files.contains(&file) {
-                failed = true;
-                println!("[FAIL] required file {file} not found in {}", rule.path_regex);
-            }
-        }
-        
-    }
-    if failed {
-        std::process::exit(2);
-    }
+    println!("meoww")
 }
